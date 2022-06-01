0f80c80e28837e064a9054e26ac72701a39f7cd7
Victordevss <102875846+Victordevss@users.noreply.github.com>
2022-06-01 10:03:28 -0300

    Delete database.js

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize')
-const connection = new Sequelize('banco', 'root','victor123',{
-    host: 'localhost',
-    dialect: 'mysql',
-    timezone: '-03:00'
-})
-
-module.exports = connection
